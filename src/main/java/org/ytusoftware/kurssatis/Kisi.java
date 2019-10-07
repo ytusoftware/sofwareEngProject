93dbc078c4d69ed525fd5ccebf30460c36e3f23e
Baris Cetinturk <baris.cetinturk.ytu@gmail.com>
2019-10-07 10:11:34 +0300

    Proje maven projesine donusturuldu

---
+++ src/main/java/org/ytusoftware/kurssatis/Kisi.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ytusoftware.kurssatis;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tekin
+ */
+public class Kisi {
+    
+    protected int id;
+    protected String ad;
+    protected String soyad;
+    protected String evTel;
+    protected String cepTel;
+    protected String adres;
+    protected String email;
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getEvTel() {
+        return evTel;
+    }
+
+    public void setEvTel(String evTel) {
+        this.evTel = evTel;
+    }
+
+    public String getCepTel() {
+        return cepTel;
+    }
+
+    public void setCepTel(String cepTel) {
+        this.cepTel = cepTel;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+
+        
+}
